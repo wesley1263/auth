1b6f1e2aea4f76722fb96de96f2568807372da8d
wesley1263 <weslei.andrade.souza@gmail.com>
2025-04-20 17:46:28 -0300

    Switch to `decouple` for environment variable management
    Replaced hardcoded settings with `decouple.config` to load values from environment variables. This improves security and flexibility by allowing configuration changes without modifying the code.

--- src/config/config.py
+++ src/config/config.py
@@ -1,12 +1,13 @@
-from pydantic import BaseSettings
+from decouple import config
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = config("EXPIRES_TOKEN_MINUTES", cast=int)
     REFRESH_TOKEN_EXPIRE_DAYS: int = 30
-    SECRET_KEY: str = "secret"
-    ALGORITHM: str = "HS256"
-    DATABASE_URL: str
+    SECRET_KEY: str = config("SECRET_KEY")
+    ALGORITHM: str = config("ALGORITHM")
+    MONGODB_URL: str = config("MONGODB_URL")
 
 
 settings = Settings()
