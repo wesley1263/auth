cc935ae1d0c0aac098116df3b32df6dd833a83c1
wesley1263 <weslei.andrade.souza@gmail.com>
2025-04-20 17:48:11 -0300

    Add UserService dependency to auth and infrastructure layers
    Introduced a `get_user_service` factory method to initialize `UserService` with `UserRepository`. Updated `get_auth_service` to include `UserService` as a dependency. This improves modularity and prepares the services for enhanced functionality.

--- src/infrastructure/dependencies.py
+++ src/infrastructure/dependencies.py
@@ -1,11 +1,16 @@
 from src.domain.services.auth_service import AuthService
 from src.domain.services.oauth_service import OAuthService
 from src.domain.services.password_reset_service import PasswordResetService
+from src.domain.services.user_service import UserService
 from src.infrastructure.repositories.user_repository import UserRepository
-
+# TODO: Move all to core/dependencies.py
 
 def get_user_repository() -> UserRepository:
     return UserRepository()
+
+
+def get_user_service() -> UserService:
+    return UserService(get_user_repository())
 
 
 def get_oauth_service() -> OAuthService:
@@ -14,7 +19,10 @@ def get_oauth_service() -> OAuthService:
 
 
 def get_auth_service() -> AuthService:
-    return AuthService()
+    return AuthService(
+        get_user_repository(),
+        get_user_service(),
+    )
 
 
 def get_password_reset_service() -> PasswordResetService:
